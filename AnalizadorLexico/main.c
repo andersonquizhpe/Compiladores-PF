09a41c084a443abd0d57ce7473fa5d233c2b5892
andersonquizhpe <jhander59@gmail.com>
2022-06-02 12:25:58 -0500

    revision

---
+++ AnalizadorLexico/main.c
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: jhander
+ *
+ * Created on 2 de junio de 2022, 11:52
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+   
+printf("  ____                      _ _           _             \n");        
+printf(" / ___|___  _ __ ___  _ __ (_) | __ _  __| | ___  _ __ ___  ___ \n");
+printf("| |   / _ \\| '_ ` _ \\| '_ \\| | |/ _` |/ _` |/ _ \\| '__/ _ \\/ __|\n");
+printf("| |__| (_) | | | | | | |_) | | | (_| | (_| | (_) | | |  __/\\__ \\\n");
+printf(" \\____\\___/|_| |_| |_| .__/|_|_|\\__,_|\\__,_|\\___/|_|  \\___||___/\n");
+printf("\n");
+printf("               Analizador Lexico          \n");
+printf("\n");
+printf("\tLuis Negrón, \n\tDavid Pacheco, \n\tAnderson Quizhpe   \n");
+return 0;
+}
+
