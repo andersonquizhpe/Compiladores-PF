64e37f554e6c973b727c66b74451a9e4c29a257f
Luis Negrón <luis.negron@unl.edu.ec>
2022-06-03 15:31:22 -0500

    obtener_token: Identificacion de espacios, tabulaciones y saltos de linea en archivo

--- AnalizadorLexico/main.c
+++ AnalizadorLexico/main.c
@@ -14,27 +14,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+//Función para obtener los componentes léxicos
+void obtener_token();
 
-printf("    _                _ _              _            \n");
-printf("   / \\   _ __   __ _| (_)______ _  __| | ___  _ __ \n");
-printf("  / _ \\ | '_ \\ / _` | | |_  / _` |/ _` |/ _ \\| '__|\n");
-printf(" / ___ \\| | | | (_| | | |/ / (_| | (_| | (_) | |   \n");
-printf("/_/   \\_\\_| |_|\\__,_|_|_/___\\__,_|\\__,_|\\___/|_| \n");
+int main(int argc, char** argv){
+	printf("    _                _ _              _            \n");
+	printf("   / \\   _ __   __ _| (_)______ _  __| | ___  _ __ \n");
+	printf("  / _ \\ | '_ \\ / _` | | |_  / _` |/ _` |/ _ \\| '__|\n");
+	printf(" / ___ \\| | | | (_| | | |/ / (_| | (_| | (_) | |   \n");
+	printf("/_/   \\_\\_| |_|\\__,_|_|_/___\\__,_|\\__,_|\\___/|_| \n");
 
-printf(" _              _           \n");
-printf("| |    _____  _(_) ___ ___  \n");
-printf("| |   / _ \\ \\/ / |/ __/ _ \\ \n");
-printf("| |__|  __/>  <| | (_| (_) |\n");
-printf("|_____\\___/_/\\_\\_|\\___\\___/ \n");
+	printf(" _              _           \n");
+	printf("| |    _____  _(_) ___ ___  \n");
+	printf("| |   / _ \\ \\/ / |/ __/ _ \\ \n");
+	printf("| |__|  __/>  <| | (_| (_) |\n");
+	printf("|_____\\___/_/\\_\\_|\\___\\___/ \n");
 
-printf("\n");
-printf("               Compiladores         \n");
-printf("\n");
-printf("\t+ Luis Negrón, \n\t+ David Pacheco, \n\t+ Anderson Quizhpe   \n");
-return 0;
+	printf("\n");
+	printf("               Compiladores         \n");
+	printf("\n");
+	printf("\t+ Luis Negrón, \n\t+ David Pacheco, \n\t+ Anderson Quizhpe   \n");
+	printf("\n");
+	
+	obtener_token();
+	
+	return 0;
 }
 
+void obtener_token(){
+	FILE *archivo;
+	char nombre_archivo[100];
+	int num_linea=1;
+	char caracter = 0;
+	
+	printf("Ingresa el nombre del archivo:\n");
+	scanf("%s", &nombre_archivo);
+	printf("El archivo que se esta analizando es: %s...\n", nombre_archivo);
+	archivo = fopen(nombre_archivo, "r");
+	
+	if (archivo == NULL){ 
+		printf ("Error al abrir el archivo");
+	}
+	while((caracter = fgetc(archivo))!= EOF){
+		if(caracter == ' ' || caracter == '\t'){
+			//Se ignoran los espacios en blanco y tabulaciones
+			continue;
+		}else if(caracter == '\n'){
+			//Se incrementa el numero de linea
+			num_linea++;
+			continue;
+		}
+		printf("%d", num_linea);
+	}
+	fclose(archivo);
+}
+	
